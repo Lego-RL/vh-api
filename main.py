c1c829dda2073c7da86a1bd276f8bb4400d156eb
Lego-RL <joeylbarroso@gmail.com>
2023-04-27 18:00:27 -0500

    test version

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
-from typing import Union
+class Comment(BaseModel):
+    timestamp: int
+    message: str
+
 
 app = FastAPI()
-
+greeting: dict = {"Hello": "World!"}
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World!"}
+    return greeting
 
+@app.post("/comment")
+async def store_comment(comment: Comment):
+    """
+    Retrieve and store single comment.
+    """
+
+    global greeting 
+    greeting = comment.dict()
+
+    return comment
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
